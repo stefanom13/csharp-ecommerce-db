a390e9eb1423c606f3161bb29492a86fd2ae5d51
stefanom13 <stefanomancuso13@gmail.com>
2022-07-12 17:47:15 +0200

    git test

--- EcommerceContentx.cs
+++
@@ -1,8 +0,0 @@
-﻿//Si realizzi un prototipo di un e-commerce con approccio
-//code-first usando Entity Framework seguendo lo schema allegato.
-//Si consideri la possibilità di poter verificare la bontà del prototipo
-//eseguendo alcune operazioni di CRUD sul sistema:
-
-internal class EcommerceContentx
-{
-}
