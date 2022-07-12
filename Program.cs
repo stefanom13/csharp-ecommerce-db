6a1de172f910e06aef0f35ea13266ae4deb22059
stefanom13 <stefanomancuso13@gmail.com>
2022-07-12 14:42:37 +0200

    creazioni classi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Si realizzi un prototipo di un e-commerce con approccio
+//code-first usando Entity Framework seguendo lo schema allegato.
+//Si consideri la possibilità di poter verificare la bontà del prototipo
+//eseguendo alcune operazioni di CRUD sul sistema:
+
+//*inserire almeno 3 prodotti diversi
+//*inserire almeno 5 ordini su almeno 2 utenti diversi
+//*recuperare la lista di tutti gli ordini effettuati da un cliente
+//*modificare l’ordine di un cliente
+//*cancellare un ordine di un cliente
+//*cancellare un prodotto su cui è attivo almeno un ordine
+
+
 Console.WriteLine("Hello, World!");
